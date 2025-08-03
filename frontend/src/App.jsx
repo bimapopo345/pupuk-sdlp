bc2f72652692cd50d1e2fcf06571a23bbeaf1e83
bimapopo345 <bimapopo345@gmail.com>
2025-08-03 16:34:05 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-white min-h-screen flex items-center justify-center">
+      <h1 className="text-red-500 text-3xl font-bold">Test Tailwind CSS</h1>
+    </div>
+  );
+}
+
+export default App;
